c26e7f5b769596aab10a1098f6482b2f4fbd4b99
Chinegua <ai.chinegua@gmail.com>
2017-10-03 12:43:48 +0200

    #2 Add Dispatcher

---
+++ src/main/java/apiDoctor/api/resource/DoctorResource.java
@@ -0,0 +1,16 @@
+package apiDoctor.api.resource;
+
+public class DoctorResource {
+
+
+	public static final String DOCTORS = "doctors";
+
+
+	public void createDoctor(String body) {
+		System.out.print("AAAA");
+
+		// TODO Auto-generated method stub
+		
+	}
+
+}
